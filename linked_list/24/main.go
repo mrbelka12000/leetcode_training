d0c722acb3cbd93a783e45ff0cccf7f282951064
beknur.karshyga <beknur.karshyga@armenotech.com>
2023-12-26 03:20:51 +0600

    problem 24 solved

--- linked_list/24/main.go
+++ linked_list/24/main.go
@@ -10,22 +10,52 @@ type ListNode struct {
 }
 
 func swapPairs(head *ListNode) *ListNode {
-	if head == nil {
-		return nil
-	}
-	var (
-		result = &ListNode{
-			Next: head,
+	curr := head
+	var last, final *ListNode
+
+	for curr != nil {
+		prev := reverse(curr, last, 2)
+		if prev == nil {
+			break
 		}
-		s = &ListNode{}
-	)
-
-	for head != nil && head.Next != nil {
-		s.Next = head
-		s = s.Next
-		head.Next = head.Next.Next
-		head = head.Next
+		if final == nil {
+			final = prev
+		}
+		last = curr
+		curr = curr.Next
 	}
 
-	return result.Next
+	return final
+}
+
+func reverse(head, last *ListNode, k int) *ListNode {
+	curr := head
+	checker := head
+	var prev, tmp *ListNode
+	var ind int
+	for checker != nil {
+		ind++
+		checker = checker.Next
+	}
+
+	for curr != nil {
+		k--
+		next := curr.Next
+		tmp = next
+		curr.Next = prev
+		prev = curr
+		curr = next
+		if k == 0 {
+			break
+		}
+	}
+
+	if last != nil {
+		last.Next = prev
+	}
+	// fmt.Printf("head=%v, start of new list=%v, last=%v, tmp=%v\n", head, prev, last, tmp)
+
+	head.Next = tmp
+
+	return prev
 }
