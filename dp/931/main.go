acd95fa161839899e105c195f424ce2156907e77
beknur.karshyga <beknur.karshyga@armenotech.com>
2024-01-19 21:33:16 +0700

    problem 931 solved

---
+++ dp/931/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(minFallingPathSum([][]int{{2, 1, 3}, {6, 5, 4}, {7, 8, 9}}))
+}
+
+var dirs = [][]int{{-1, 0}, {-1, -1}, {-1, 1}}
+
+func minFallingPathSum(matrix [][]int) int {
+	mp := initMap(matrix)
+	for i := 1; i < len(matrix); i++ {
+		for j := 0; j < len(matrix); j++ {
+			minVal := math.MaxInt32
+			for _, dir := range dirs {
+				jj := j + dir[1]
+				val, ok := mp[i-1][jj]
+				if !ok {
+					continue
+				}
+				minVal = min(minVal, val+matrix[i][j])
+			}
+			mp[i][j] = minVal
+		}
+	}
+	var result int = math.MaxInt32
+
+	for _, v := range mp[len(matrix)-1] {
+		result = min(result, v)
+	}
+
+	fmt.Println(mp)
+
+	return result
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func initMap(matrix [][]int) map[int]map[int]int {
+	mp := make(map[int]map[int]int)
+
+	for i := 0; i < len(matrix); i++ {
+		mp[i] = make(map[int]int)
+		mp[0][i] = matrix[0][i]
+	}
+	return mp
+}
