185858e24f65addc269038ca8cdab97a06f1f37c
beknur.karshyga <beknur.karshyga@armenotech.com>
2024-04-27 01:39:53 +0500

    problem 316 solved

---
+++ greedy/316/ReadME.md
@@ -0,0 +1,33 @@
+# 316. Remove Duplicate Letters
+
+## SOLVED
+### https://leetcode.com/problems/remove-duplicate-letters/description/
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+
+### Example 1:
+
+
+Input: s = &quot;bcabc&quot;
+
+Output: &quot;abc&quot;
+
+
+### Example 2:
+
+
+Input: s = &quot;cbacdcbc&quot;
+
+Output: &quot;acdb&quot;
+
+
+
+Constraints:
+
+
+	1 <= s.length <= 104
+	s consists of lowercase English letters.
+
+
+
+Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
---
+++ greedy/316/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//fmt.Println(removeDuplicateLetters("cbacdcbc"))
+	fmt.Println(removeDuplicateLetters("rusrbofeggbbkyuyjsrzornpdguwzizqszpbicdquakqws"))
+}
+
+func removeDuplicateLetters(s string) string {
+	freq := make(map[rune]int)
+
+	for _, v := range s {
+		freq[v]++
+	}
+
+	// fmt.Println(freq)
+	present := make(map[rune]bool)
+	var result []rune
+
+	for _, c := range s {
+		if !present[c] {
+			for len(result) > 0 && result[len(result)-1] > c && freq[result[len(result)-1]] > 0 {
+				present[result[len(result)-1]] = false
+				result = result[:len(result)-1]
+			}
+
+			result = append(result, c)
+			present[c] = true
+		}
+		freq[c]--
+	}
+
+	return string(result)
+}
